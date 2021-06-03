5c85eb5d4314b3670f284284cbae03fb013e821d
nadeem-114 <33491661+nadeem-114@users.noreply.github.com>
2021-06-04 02:52:02 +0500

    Create index.js

---
+++ Front_End/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+/*import {
+  BrowserRouter,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";*/
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
