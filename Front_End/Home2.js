c5f4d43c55c7836ac0b59c9dfb340ab919eb2e5b
nadeem-114 <33491661+nadeem-114@users.noreply.github.com>
2021-06-04 02:50:57 +0500

    Create Home2.js

---
+++ Front_End/Home2.js
@@ -0,0 +1,96 @@
+import React from "react";
+//import styled from "styled-components";
+//import ReactDOM from "react-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../src/styles/navbarStyling.css";
+
+const Home2 = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm">
+          <div class="card">
+            <img
+              class="card-img-top"
+              src="https://apollo-singapore.akamaized.net/v1/files/9z1rkv0rpkt-PK/image;s=1080x1080"
+              alt="Card cap"
+              height="300px"
+            />
+            <div class="card-body">
+              <p class="card-text">Base Price: 3500000</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm">
+          <div class="card">
+            <img
+              class="card-img-top"
+              src="https://apollo-singapore.akamaized.net/v1/files/h0j4c6f7pv581-PK/image;s=1080x1080"
+              alt="Card cap"
+              height="300px"
+            />
+            <div class="card-body">
+              <p class="card-text">Base Price: 2000000</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm">
+          <div class="card">
+            <img
+              class="card-img-top"
+              src="https://fsb.zobj.net/crop.php?r=u63BwUuWhFUjDSLvLBsELpEIwdzPHkiW9dDyNiyNyI4XuHNqaz7wdc7cKxrvuuVxYiG8dXnzy1YlPQwEiJvf3eji3lxjy7J0aBpxk71WqBgtNW6PbXi_PRBWL39C51CRuSiqJ8apzHCUiZ8d"
+              alt="Card cap"
+              height="300px"
+            />
+            <div class="card-body">
+              <p class="card-text">Base Price: 2500000</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm">
+          <div class="card">
+            <img
+              class="card-img-top"
+              src="https://apollo-singapore.akamaized.net/v1/files/9z1rkv0rpkt-PK/image;s=1080x1080"
+              alt="Card cap"
+              height="300px"
+            />
+            <div class="card-body">
+              <p class="card-text">Base Price: 3500000</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm">
+          <div class="card">
+            <img
+              class="card-img-top"
+              src="https://apollo-singapore.akamaized.net/v1/files/h0j4c6f7pv581-PK/image;s=1080x1080"
+              alt="Card cap"
+              height="300px"
+            />
+            <div class="card-body">
+              <p class="card-text">Base Price: 2000000</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm">
+          <div class="card">
+            <img
+              class="card-img-top"
+              src="https://fsb.zobj.net/crop.php?r=u63BwUuWhFUjDSLvLBsELpEIwdzPHkiW9dDyNiyNyI4XuHNqaz7wdc7cKxrvuuVxYiG8dXnzy1YlPQwEiJvf3eji3lxjy7J0aBpxk71WqBgtNW6PbXi_PRBWL39C51CRuSiqJ8apzHCUiZ8d"
+              alt="Card cap"
+              height="300px"
+            />
+            <div class="card-body">
+              <p class="card-text">Base Price: 2500000</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home2;
